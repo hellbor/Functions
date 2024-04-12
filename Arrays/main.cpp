ceb86d670d2a89bf32f734a0b3df15d21c8a9fc5
Usova_le12@top-academy.ru <Usova_le12@top-academy.ru>
2024-04-12 23:09:54 +0300

    Arrays DONE

---
+++ Arrays/main.cpp
@@ -0,0 +1,138 @@
+﻿#include<iostream>
+using std::cin;
+using std::cout;
+using std::endl;
+
+#define tab "\t"
+
+void FillRand(int arr[], const int n);
+void Print(const int arr[], const int n);
+void Sort(int arr[], const int n);
+void Sum(int arr[], const int n);
+void Avg(int arr[], const int n);
+void MinValueIn(int arr[], const int n);
+void MaxValueIn(int arr[], const int n);
+void ShiftLeft(int arr[], const int n);
+void ShiftRight(int arr[], const int n);
+
+void main()
+{
+	setlocale(LC_ALL, "");
+	const int n = 5;
+	int arr[n];
+	FillRand(arr, n);
+	Print(arr, n);
+	Sort(arr, n);
+	Print(arr, n);
+	Sum(arr, n);
+	Avg(arr, n);
+	MinValueIn(arr, n);
+	MaxValueIn(arr, n);
+	ShiftLeft(arr, n);
+	Print(arr, n);
+	ShiftRight(arr, n);
+	Print(arr, n);
+}
+
+void FillRand(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		arr[i] = rand() % 100;
+	}
+}
+void Print(const int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		cout << arr[i] << tab;
+	}
+	cout << endl;
+}
+void Sort(int arr[], const int n)
+{
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[i])
+			{
+				int buffer = arr[i];
+				arr[i] = arr[j];
+				arr[j] = buffer;
+			}
+		}
+	}
+}
+void Sum(int arr[], const int n)
+{
+	int sum = 0;
+	for (int i = 0; i < n; i++)
+	{
+		sum += arr[i];
+		cout << sum << tab;
+	}
+	cout << endl;
+}
+void Avg(int arr[], const int n)
+{
+	int sum = 0;
+	{
+		for (int i = 0; i < n; i++)
+		{
+			sum += arr[i];
+			cout << (double)sum / n << tab;
+		}
+		cout << endl;
+	}
+}
+void MinValueIn(int arr[], const int n)
+{
+	int min;
+	min = arr[0];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] < min)min = arr[i];
+		cout << min << tab;
+	}
+	cout << endl;
+}
+void MaxValueIn(int arr[], const int n)
+{
+	int max;
+	max = arr[4];
+	for (int i = 0; i < n; i++)
+	{
+		if (arr[i] > max)max = arr[i];
+		cout << max << tab;
+	}
+	cout << endl;
+}
+void ShiftLeft(int arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "Введите количество сдвигов влево:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[0];
+		for (int i = 1; i < n; i++)
+		{
+			arr[i - 1] = arr[i];
+		}
+		arr[n - 1] = buffer;
+	}
+}
+void ShiftRight(int arr[], const int n)
+{
+	int number_of_shifts;
+	cout << "Введите кличество сдвигов вправо:"; cin >> number_of_shifts;
+	for (int i = 0; i < number_of_shifts; i++)
+	{
+		int buffer = arr[n - 1];
+		for (int i = n - 1; i > 0; i--)
+		{
+			arr[i] = arr[i - 1];
+		}
+		arr[0] = buffer;
+	}
+}
